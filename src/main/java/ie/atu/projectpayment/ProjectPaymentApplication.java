2cb7256210e2ecd6131622ecdcf09d5e985e8edc
Johnson Shogbaike <g00389668@atu.ie>
2023-12-28 16:52:35 +0000

    Created new microservice called Payment

---
+++ src/main/java/ie/atu/projectpayment/ProjectPaymentApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.projectpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPaymentApplication.class, args);
+    }
+
+}
